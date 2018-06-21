617ca86d537290f599e72e5e35a7c2fb166353e4
fcmarina <fcmarina21@gmail.com>
2018-06-21 20:06:22 -0300

    Primeiro teste

---
+++ tests/HtmlTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace fcmarina\Solid;
+
+/**
+ * 
+ */
+class HtmlTest extends \PHPUnit\Framework\TestCase
+{
+	
+	public function testPrimeiroTeste()
+	{
+		$this->assertTrue(true);
+	}
+}
+  ?>
