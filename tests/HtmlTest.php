73fa68ec8d42afb9af34f0dbfb6d1472ba2ecb24
fcmarina <fcmarina21@gmail.com>
2018-06-22 13:53:30 -0300

    Criando teste da classe ui

--- tests/HtmlTest.php
+++ tests/HtmlTest.php
@@ -20,9 +20,19 @@ class HtmlTest extends \PHPUnit\Framework\TestCase
 	{
 		$html = new Html;
 		$img = $html->img('images/photo.jpg');
-
 		$link = $html->a('http://seusite.com/perfil', $img);
 
+
 		$this->assertEquals('<a href= "http://seusite.com/perfil"><img src="images/photo.jpg"></a>', $link);
+	}
+
+	public function testCriaUmaLista()
+	{
+		$html = new Html;
+
+		$list = $html->uI('<li>Marina Ferreira</li>');
+
+		// O que é esperado
+		$this->assertEquals('<ul><li>Marina Ferreira</li></ul>', $list);
 	}
 }
